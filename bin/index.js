7a0011ee0ca072c6de5f0694a35e1523222cd7cd
karan1798 <76094880+karan1798@users.noreply.github.com>
2022-07-08 15:02:15 +0530

    dependencies showed in table

--- bin/index.js
+++ bin/index.js
@@ -2,4 +2,18 @@
 // console.log("Hello World!");
 
 const packages=require("../package.json")
-console.log(packages.dependencies);
+
+
+
+let packages_arr=[]
+
+for(let i of Object.keys(packages.dependencies)){
+    let pakageDetails={
+        name:"",
+        version:""
+    }
+    pakageDetails.name=i;
+    pakageDetails.version=packages.dependencies[i]
+    packages_arr.push(pakageDetails)
+}
+console.table(packages_arr);
